a68b05912193bbc099bd2c795bc113fb6707587c
Makayla Clausen <mclausen792@gmail.com>
2018-08-22 10:42:43 -0600

    Access control header

--- app.go
+++ app.go
@@ -79,6 +79,7 @@ func main() {
 	r.HandleFunc("/guests", CreateGuestEndPoint).Methods("POST")
 	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "application/json")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Write([]byte("{\"hello\": \"world\"}"))
 	})
 
